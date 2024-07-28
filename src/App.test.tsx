ed6e1b705a00d73a77fb416be90af443bce89a88
Vasyl Pashko <vasyl.pashko@gmail.com>
2024-07-28 11:10:29 +0300

    add simple react project

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { act } from "react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+test("renders hello world", async () => {
+  await act(async () => {
+    render(<App />);
+  });
+  const linkElement = screen.getByText(/Hello World!/i);
   expect(linkElement).toBeInTheDocument();
 });
